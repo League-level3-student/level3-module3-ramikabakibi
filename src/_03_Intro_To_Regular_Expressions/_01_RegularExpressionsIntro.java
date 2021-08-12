bc538fe28eb1b6b4216fbfcf4bcdf52a70fb4b6e
student <student@ip-172-31-54-81.us-west-2.compute.internal>
2021-08-11 18:28:30 -0700

    Finished Regular Expressions

--- src/_03_Intro_To_Regular_Expressions/_01_RegularExpressionsIntro.java
+++ src/_03_Intro_To_Regular_Expressions/_01_RegularExpressionsIntro.java
@@ -17,6 +17,10 @@ public class _01_RegularExpressionsIntro {
      * Code method below! 
      */
     String emailRemover(String message) {
-        return null;
+    	
+    	String regEx="@[\\w]+\\.com";
+    	String output="";
+    	output=message.replaceAll(regEx, "@mail.com");
+        return output;
     }
 }
